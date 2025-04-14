7a63c212aa3f95828489f572ea08a8c0aa5edef4
sarajy04 <sarafuahjy@gmail.com>
2025-04-14 20:07:37 +0800

    -

--- dashboard.py
+++ dashboard.py
@@ -662,16 +662,16 @@ else:
         ax.set_ylabel("Predicted Sales")
         ax.set_title("Actual vs Predicted Sales (All Models)")
         ax.legend()
-        col1, col2 = st.columns([2, 1])  # Plot and overview side-by-side
+        col1, col2 = st.columns([2, 1]) 
 
         with col1:
             st.pyplot(fig)
 
         with col2:
             st.markdown("""
-            ### 📌 Overview: Actual vs Predicted Sales
+            ### Overview: Actual vs Predicted Sales
             - **All models** show a concentration of predictions at lower sales values.
-            - **High actual sales values** are often **underpredicted**.
+            - **Low actual sales values** are **overpredicted**.
             - Clustered predictions near origin suggest conservative estimations.
             - Outliers: Some actuals > \$15,000 are predicted far lower.
             - Strong overlap among models indicates similar trends.
@@ -693,7 +693,7 @@ else:
         st.table(metrics_df)
         st.markdown("""
         **🔍 Performance Summary**
-        -  **LightGBM** is the top performer (Lowest MSE & RMSE, Highest R²)
+        -  **LightGBM** is the **top performer** (Lowest MSE & RMSE, Highest R²)
         -  **HistGradBoost** and **XGBoost** also show strong results
         -  **Random Forest** shows the highest error metrics
         -  Boosting models outperform traditional Random Forest in this case
@@ -706,12 +706,13 @@ else:
         col1, col2 = st.columns(2)
         with col1:
             postal_code = st.selectbox("Postal Code", df['Postal Code'].unique())
-            sub_category = st.selectbox("Sub-Category", df['Sub-Category'].unique())
-            region = st.selectbox("Region", df['Region'].unique())
-        with col2:
-            segment = st.selectbox("Customer Segment", df['Segment'].unique())
-            ship_mode = st.selectbox("Shipping Mode", df['Ship Mode'].unique())
 
+        with col2:
+            region = st.selectbox("Region", df['Region'].unique())  
+
+        segment = st.selectbox("Customer Segment", df['Segment'].unique())
+        sub_category = st.selectbox("Sub-Category", df['Sub-Category'].unique())
+        ship_mode = st.selectbox("Shipping Mode", df['Ship Mode'].unique())
         model_choice = st.selectbox("Select Model", list(trained_models.keys()))
         submit_button = st.form_submit_button("Predict Sales")
 
@@ -744,47 +745,47 @@ else:
 
         if model_choice == "Random Forest":
             st.markdown("""
-            **Random Forest Overview:**
-            - General Trend : The points cluster around the diagonal, showing good alignment between actual and predicted sales.
-            - Accuracy : The model appears slightly more spread out compared to LightGBM, indicating a bit more variability in predictions.
-            - Outliers : A few outliers are visible, particularly at higher actual sales values. 
-            - Range : Covers a broad range of sales values, but with slightly more deviation from the diagonal compared to LightGBM.
+            ✅**Random Forest Overview:**
+            - **General Trend** : The points cluster around the diagonal, showing good alignment between actual and predicted sales.
+            - **Accuracy** : The predictions appears slightly more spread out compared to the rest, indicating a bit more variability in predictions.
+            - **Outliers** : A few outliers are visible, particularly at higher actual sales values. 
+            - **Range** : Covers a broad range of sales values, but with slightly more deviation from the diagonal compared to other models.
             """)
 
         elif model_choice == "Gradient Boosting":
             st.markdown("""
-             ** Gradient Boosting Overview:**
-            - General Trend : The points are densely clustered around the diagonal, showing good alignment between actual and predicted sales.
-            - Accuracy : High accuracy, with most predictions closely matching actual sales.
-            - Outliers : Fewer noticeable outliers compared to Random Forest, suggesting more consistent predictions.
-            - Range : Effective prediction across a wide range of sales values, with a slight tendency to underestimate at higher actual sales levels.
+            ✅ **Gradient Boosting Overview:**
+            - **General Trend** : The points are clustered around the diagonal, showing good alignment between actual and predicted sales.
+            - **Accuracy** : High accuracy, with most predictions closely matching actual sales.
+            - **Outliers** : Fewer noticeable outliers compared to Random Forest, suggesting more consistent predictions.
+            - **Range** : Effective prediction across a wide range of sales values, with a slight tendency to underestimate at higher actual sales levels.
             """)
 
         elif model_choice == "HistGradientBoosting":
             st.markdown("""
-            **HistGradientBoost Overview:**
-            - General Trend : Points are tightly clustered around the diagonal, indicating strong alignment between actual and predicted sales.
-            - Accuracy : Very high accuracy, with minimal deviation from the diagonal.
-            - Outliers : Almost no significant outliers, suggesting robust performance across the dataset.
-            - Range : Consistent predictions across all sales ranges, with excellent coverage of both low and high values.
+            ✅**HistGradientBoost Overview:**
+            - **General Trend** : Points are tightly clustered around the diagonal, indicating good alignment between actual and predicted sales.
+            - **Accuracy** : High accuracy, with minimal deviation from the diagonal.
+            - **Outliers** : Almost no significant outliers, suggesting robust performance across the dataset.
+            - **Range** :Effective prediction across a broad range of sales values, with good coverage of both low and high sales figures. 
             """)
 
         elif model_choice == "XGBoost":
             st.markdown("""
-            **XGBoost Overview:**
-            - General Trend : The points follow a clear diagonal pattern, indicating good predictive performance.
-            - Accuracy : High accuracy, with most predictions closely aligned with actual sales.
-            - Outliers : Some minor deviations are visible, particularly at higher actual sales values, where the model may slightly overpredict.
-            - Range : Effective prediction across a broad range of sales values, with good coverage of both low and high sales figures.
+            ✅**XGBoost Overview:**
+            - **General Trend** : The points follow a clear diagonal pattern, indicating good predictive performance.
+            - **Accuracy** : High accuracy, with most predictions closely aligned with actual sales.
+            - **Outliers** : Some minor deviations are visible, particularly at higher actual sales values, where the model may slightly overpredict.
+            - **Range** : Effective prediction across a broad range of sales values, with good coverage of both low and high sales figures.
             """)
 
         elif model_choice == "LightGBM":
             st.markdown("""
-            **LightGBM Overview:**
-            - General Trend : The points are clustered around a diagonal line, indicating that the predicted values generally align well with the actual sales.
-            - Accuracy : High accuracy.
-            - Outliers : There are some points scattered away from the diagonal, indicating potential overestimation or underestimation for certain data points.
-            - Range : The model predicts sales across a wide range, capturing both low and high actual sales values effectively.
+            🏅**LightGBM Overview:**
+            - **General Trend** : The points are densely clustered around a diagonal line, indicating good predictive performance.
+            - **Accuracy** : Highest accuracy, with most predictions closely aligned with actual sales.
+            - **Outliers** : Only minor deviations from the diagonal are visible, indicating potential overestimation or underestimation for certain data points.
+            - **Range** : Effective prediction across a broad range of sales values, with good coverage of both low and high sales figures.
             """)
 
 # Footer
